1c3c19131624a80c1875998b9e0ede43d447f7bd
Arun Kumar <dahiya.akd94@gmail.com>
2023-02-25 11:12:54 +0530

    Added UserRating model and introduced new API

--- src/main/java/com/java/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/com/java/ratingsdataservice/resources/RatingsResource.java
@@ -1,9 +1,13 @@
 package com.java.ratingsdataservice.resources;
 
 import com.java.ratingsdataservice.models.Rating;
+import com.java.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -14,4 +18,20 @@ public class RatingsResource {
 
         return new Rating(movieId, 4);
     }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        /** using a list as return type is not recommended. Better to put that list in an object and pass */
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123", 4),
+                new Rating("124", 3));
+
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+
+        return userRating;
+
+
+    }
+
 }
